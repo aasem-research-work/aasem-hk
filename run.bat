849782025a87fb2bd116ce0f8022918ca2fa4c8a
aasem-research-work <101444683+aasem-research-work@users.noreply.github.com>
2023-09-08 21:30:38 +0500

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+:: -------------------------------------------------------------------------
+:: Batch script to activate the pyqt5 environment and run main.py
+:: -------------------------------------------------------------------------
+
+:: Activate the pyqt5 environment
+call conda activate pyqt5
+
+:: Run main.py
+python main.py
+
+:: End the script
+endlocal
+pause
